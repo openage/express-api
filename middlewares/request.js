6635b457cc4261e4271bd78cdc88b0e3d7c15b8e
Vijay Swarnkar <YATRAONLINE\vijay.swarnkar@lap0003509.yatraonline.local>
2023-03-13 12:59:27 +0530

    unnecessary code removed

--- middlewares/request.js
+++ middlewares/request.js
@@ -111,7 +111,7 @@ const decorateResponse = (res, context, log) => {
         }
 
         // TODO cache here
-        if (context.cache && context.cache.httpAction == "GET" && context.cache.action == "add")
+        if (context.cache.httpAction == "GET" && context.cache.action == "add" && context.cache.key)
             context.cache.set(`${context.service}:${context.cache.key}`, val, context.cache.ttl)
 
         res.json(val)
